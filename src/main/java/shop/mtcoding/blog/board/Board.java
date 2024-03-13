792c1ce6b04f8e0d9a28b5dffe80bdc416f69f45
minmeanmin <wed1604@naver.com>
2024-03-13 17:29:02 +0900

    게시글 상세보기 완료

--- src/main/java/shop/mtcoding/blog/board/Board.java
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -1,13 +1,15 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import shop.mtcoding.blog.user.User;
 
 import java.sql.Timestamp;
 
-@NoArgsConstructor
+@NoArgsConstructor //엔티티는 디폴트 생성자 무조건 있어야 함
 @Data
 @Table(name = "board_tb")
 @Entity
@@ -17,8 +19,20 @@ public class Board {
     private Integer id;
     private String title;
     private String content;
-    private String username;
 
-    @CreationTimestamp // pc -> db (날짜주입)
+    //@JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user; //db -> user_id
+
+    @CreationTimestamp // pc -> db (날짜가 주입됨)
     private Timestamp createdAt;
+
+    @Builder
+    public Board(Integer id, String title, String content, User user, Timestamp createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.createdAt = createdAt;
+    }
 }
